931bab4cebf1a3189a2e730c212c89dba614cdb3
mohamedessaks <58851212+mohamedessaks@users.noreply.github.com>
2019-12-26 11:21:23 -0800

    ProjectManer   UI Basic changes

---
+++ ProjectManagerUI/src/app/users/users.component.spec.ts
@@ -0,0 +1,23 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UsersComponent } from './users.component';
+import {HttpClientModule} from '@angular/common/http';
+
+describe('UsersComponent', () => {
+  let component: UsersComponent;
+  let fixture: ComponentFixture<UsersComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ UsersComponent ],
+      providers:[HttpClientModule]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UsersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+});
